7bad0119c106bcd6c91252f730fbcd051423e15c
samqi <samng.qi@gmail.com>
2026-06-03 17:19:59 +0800

    Google Analytics

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="en-us" class="pf-theme-dark">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RK15PBPZ2F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RK15PBPZ2F');
+</script>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Digital Sovereignty Readiness Assessment</title>
